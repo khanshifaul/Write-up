56fe616cfad62a76d371bc3fa27e46f095a55441
Shifaul Islam <khanshifaul@gmail.com>
2022-08-19 04:55:34 +0600

    commit

---
+++ Reverse_Engineering/00.Testing/testing.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+	printf("Printing with printf(). Here is some data: %d, %d, %f, %p, %d, %f, %f\n", 4, 2, 1.0f, (void*)0xFFF7865, 1498, 87.003f, 99123.34);
+	return 0;
+}
